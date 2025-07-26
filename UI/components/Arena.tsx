7fe7c72ad695a6a7cafc15c0761d13deec7c29b9
Oscar G <oscar@bloodandtreasure.com>
2025-07-26 13:49:30 -0400

    Updated UI

--- UI/components/Arena.tsx
+++ UI/components/Arena.tsx
@@ -4,7 +4,7 @@ import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { Badge } from "./ui/badge";
 import { ArrowLeft, Clock, Send, Trophy, ExternalLink, CheckCircle, Lock, ArrowDown, ArrowRight } from "lucide-react";
-import { motion } from "motion/react";
+import { motion } from "framer-motion";
 
 interface ArenaProps {
   roomName: string;
